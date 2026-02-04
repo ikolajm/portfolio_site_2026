efb316a61bbf9e6faf3efcc360487d5127f5e630
ikolajm <ikolajm@gmail.com>
2026-02-04 10:16:00 -0500

    Experimentations with waves for nonblur

--- js/wave.js
+++ js/wave.js
@@ -14,17 +14,20 @@ const renderer = new THREE.WebGLRenderer({
     alpha: true, 
     antialias: true
 });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(innerWidth, innerHeight);
 renderer.setAnimationLoop( animationLoop );
 const wave = document.getElementById('wave');
 wave.appendChild(renderer.domElement);
 			
 window.addEventListener( "resize", (event) => {
     camera.aspect = innerWidth/innerHeight;
+    renderer.setSize(innerWidth, innerHeight);
     camera.updateProjectionMatrix();
 });
 
 // Waves with simplex noise
-const geometry = new THREE.PlaneGeometry(innerWidth/8, 1, innerWidth*2, 4);
+const geometry = new THREE.PlaneGeometry(innerWidth/10, 1, innerWidth*2, 4);
 const pos = geometry.getAttribute('position');
 const simplex = new SimplexNoise();
 
@@ -32,17 +35,18 @@ const waves = new THREE.Points(
     geometry,
     new THREE.PointsMaterial({
         color: "#E4E6E7",
-        size: .1
+        size: .025 * window.devicePixelRatio
     })
 );	
-waves.rotation.x = Math.PI/8;
+waves.rotation.x = .25;
+waves.rotation.z = .15;
 scene.add(waves);
 
 function animationLoop( t ) {
     for( let i=0; i<pos.count; i++ ) {
         const x = pos.getX( i );
         const y = pos.getY( i );	
-        const z = simplex.noise3d( x/2, y/2, t/5000 );
+        const z = simplex.noise3d( x/2, y/2, t/8000 );
 
         pos.setZ( i, z );
     }
